45545f7d65f5db4261509d0501c54495665c3de5
marthed <martinhedlund@live.com>
2018-02-19 20:38:15 +0100

    uglify options

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,9 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
    plugins: [
-     new UglifyJSPlugin(),
+     new UglifyJSPlugin({
+      uglifyOptions: { ecma: 8 }
+     }),
      new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
