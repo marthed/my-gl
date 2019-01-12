b3d5f5c4aa622e6e874057499a319ee80765f8e5
marthed <martinhedlund@live.com>
2019-01-12 23:35:15 +0100

    Functioning gitignore. Remove unneccesary clear parameter

--- client/app.js
+++ client/app.js
@@ -20,7 +20,7 @@ var runDemo = function (vertexShaderText, fragmentShaderText, susanImage, model)
   }
 
   gl.clearColor(0.75, 0.85, 0.8, 1.0); // set color of paint
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT);
   gl.enable(gl.DEPTH_TEST);
   gl.enable(gl.CULL_FACE);
   gl.frontFace(gl.CCW);
