9afc7f05129ec616d9de14e5bc00f725ce75c456
marthed <martinhedlund@live.com>
2019-01-14 22:59:21 +0100

    Added fragment shader. Fixed some things

--- client/entry.js
+++ client/entry.js
@@ -8,22 +8,31 @@ const drawLoop = function() {
   gl.clearColor(0.75, 0.85, 0.8, 1.0);
   gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.POINTS, 5, 8, 4);
+  gl.drawArrays(gl.POINTS, 0, 10);
 
   requestAnimationFrame(drawLoop);
 };
 
-function setupProgram(shader) {
+function setupProgram(shader1, shader2) {
   const program = gl.createProgram();
-  gl.attachShader(program, shader);
+  gl.attachShader(program, shader1);
+  gl.attachShader(program, shader2);
   //Linking a vertex shader and a fragment shader into a single program makes sure that both programs reference the same global variables.
-  //gl.linkProgram();
-  gl.useProgram(program);
+  gl.linkProgram(program);
+  const programIsLinked = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (programIsLinked) {
+    gl.useProgram(program);
+    return program;
+  }
+  else {
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
+  
 }
 
 function createShader(type, source) {
   const shader = gl.createShader(type);
-  console.log('shader: ', shader);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
   const shaderHasCompiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
@@ -42,13 +51,30 @@ function checkBrowserSupportForGL() {
   }
 }
 
+const positions = [
+  0, 0, 0.3,
+  0, 0.3, 0.3, 
+  0.7, 0.2, 1
+]
+
 async function main() {
   try {
     checkBrowserSupportForGL();
     const vertexShaderSource = await getTextResource('./shaders/shader.vs.glsl');
-    const vertexShader = createShader(gl.VERTEX_SHADER, vertexShaderSource); // gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
-    console.log('vertexShader: ', vertexShader);
-    setupProgram(vertexShader);
+    const fragmentShaderSource = await getTextResource('./shaders/shader.fs.glsl');
+    const vertexShader = createShader(gl.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = createShader(gl.FRAGMENT_SHADER, fragmentShaderSource)
+    const program = setupProgram(vertexShader, fragmentShader);
+
+    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    
+
+
+
     drawLoop();
   } catch (e) {
     console.log(e);
